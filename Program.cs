cd71b9840756900ec41a6ad7387fbd4de89bce4e
Andrey Vershinsky <vershinsky90@gmail.com>
2023-05-31 21:24:22 +0300

    Task2

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{Console.Write("max="); Console.WriteLine(number1);}
+else 
+{Console.Write("max="); Console.WriteLine(number2);}
